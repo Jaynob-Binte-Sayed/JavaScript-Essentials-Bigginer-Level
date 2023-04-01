750f1b2da1e2d10fc5558cd74176398289e1a8b4
Jaynob <jynobpapiya12@gmail.com>
2023-04-02 00:34:06 +0600

    Added Module 2

---
+++ JSE1 (Module base Practise)/Module 1/app.js
@@ -0,0 +1,19 @@
+// Task 1
+// Use console.log to output your full name to the console.
+
+console.log("Jaynob Binte Sayed");
+
+// Task 2
+// Output your year of birth.
+
+console.log("1996");
+
+// Task 3
+// Try again to output your year of birth, this time passing the date without the quotes.
+
+console.log(1996);
+
+// Task 4
+// Output information about yourself to the console in the format: Name Surname (Year) e.g. Mary Stuart (1542).
+
+console.log("Jaynob, Sayed, 1996");
