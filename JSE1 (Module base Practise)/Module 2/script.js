647a6006d03b686b59da2291db3b62466be0f868
Jaynob <jynobpapiya12@gmail.com>
2023-04-04 09:36:08 +0600

    Module 1 has ended here at home

---
+++ JSE1 (Module base Practise)/Module 2/script.js
@@ -0,0 +1,32 @@
+// var height;
+// console.log(height);
+// console.log(weight);
+
+// use of let and var
+
+// let height;
+// let height;
+
+
+// var height;
+// var height;
+
+
+// let height = 180;
+// let anotherHeight = height;
+// console.log(height);
+// console.log(anotherHeight);
+// let weight = 70;
+// console.log(weight);
+
+"use strict";
+
+// height = 180;
+// console.log(height);
+
+let steps = 100;
+console.log(steps);
+steps = 120;
+console.log(steps);
+steps = steps + 200;
+console.log(steps);
