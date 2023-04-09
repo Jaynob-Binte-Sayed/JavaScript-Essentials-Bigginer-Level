2800ae3fe463ad6f259e3f1c0fb42eba85f23110
Jaynob <jynobpapiya12@gmail.com>
2023-04-09 17:47:32 +0600

    Module 3 completed

--- JSE1 (Module base Practise)/Module 3/script.js
+++ JSE1 (Module base Practise)/Module 3/script.js
@@ -133,7 +133,137 @@ let empty = "";
 
 
 
-console.log(true || false);  
-console.log(false || false);  
-console.log(false && false && true);  
-console.log(true || false || false && true);
+// console.log(true || false);  
+// console.log(false || false);  
+// console.log(false && false && true);  
+// console.log(true || false || false && true);
+
+
+let greetings = "Hi";
+let sen = "Alice";
+// let newSentence = greetings + " " + sen;
+// console.log(newSentence);
+
+// let sentence = "Happy New Year";
+// let sentenceNew = sentence + " 1996";
+// console.log(sentenceNew);
+
+let sentence = "Happy New ";
+sentence += "Year";
+// console.log(sentence);
+sentence += " 2023"
+// console.log(sentence);
+
+
+// alert("Hey! Ramjam Mubarak");
+// alert(4*7);
+// alert(true);
+// alert("text 1", "text 2");
+
+
+
+// let declaration = window.confirm("Is it ok?");
+// console.log(declaration);
+
+
+// let rmv = confirm("Remove All the daya");
+// let message = rmv ? "Deleting Data" : "Cancelled operation";
+
+// console.log(message);
+
+
+// let name = window.prompt("What is your name?", "John Doe");
+// name = name ? name : "anonymous";
+
+// let age = prompt("What is your age " + name);
+// alert(name + " is " +  age + " years old");
+
+
+
+// let length = prompt("Enter the length:");
+// console.log(length);
+
+// let width = prompt("Enter the width:");
+// console.log(width);
+
+// let height = prompt("Enter the height:");
+// console.log(height);
+
+
+// console.log(length * width * height);
+
+
+
+// let contacts = [{
+//     name: "Maxwell Wright",
+//     phone: "(0191) 719 6495",
+//     email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+//     }, {
+//     name: "Raja Villarreal",
+//     phone: "0866 398 2895",
+//     email: "posuere.vulputate@sed.com"
+//     }, {
+//     name: "Helen Richards",
+//     phone: "0800 1111",
+//     email: "libero@convallis.edu"
+//     }];
+    
+//     // write your code here
+
+//     let newName = prompt("Enter your name:");
+
+//     let newPhone = prompt("Enter your phone:");
+    
+//     let newEmail = prompt("Enter your email:");
+
+// contacts[2] = {
+//     name: newName,
+//     phone: newPhone, 
+//     email: newEmail
+
+// };
+
+    
+// let last = contacts.length - 1;
+    
+// console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
+// console.log(`${contacts[last].name} / ${contacts[last].phone} / ${contacts[last].email}`);
+
+
+// let test = prompt("Hello", "World");
+// console.log(test);
+
+// let n = 10;
+// let m = ++n;
+
+// console.log(n);
+// console.log(m);
+
+
+// let strg = "12";
+// strg = +strg;
+
+// console.log(strg);
+
+let n = 2 * 3 ** 3 -1 ;
+// n **= 3;
+console.log(n);
+
+
+let nr = "1";
+
+let g = (nr === 1);
+console.log(g);
+let h = (nr == 1);
+console.log(h);
+
+let i = (nr = 1);
+console.log(i);
+
+
+let z = false || "false";
+
+// console.log(z);
+
+let f = 20|| 5;
+console.log(f);
