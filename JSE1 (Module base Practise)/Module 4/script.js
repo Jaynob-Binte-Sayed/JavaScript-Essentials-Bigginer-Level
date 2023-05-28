6bb4270ab3597d0eacdc9a19017839eee72d3b92
Jaynob <jynobpapiya12@gmail.com>
2023-05-28 18:49:36 +0600

    Module 4 is going on

--- JSE1 (Module base Practise)/Module 4/script.js
+++ JSE1 (Module base Practise)/Module 4/script.js
@@ -1,20 +1,19 @@
-<<<<<<< HEAD
+
 // let isUserReady = confirm("Are you ready?");
 // console.log(isUserReady);
 // if ( isUserReady ) {
 //     alert ("I am ready");
 // }
 
-let UnitPrice = 10;
-let pieces = prompt("How many pieces do you want?", 0);
-if ( pieces > 0 ) {
-    let total = UnitPrice * pieces;
-    console.log(total);
-} 
+// let UnitPrice = 10;
+// let pieces = prompt("How many pieces do you want?", 0);
+// if ( pieces > 0 ) {
+//     let total = UnitPrice * pieces;
+//     console.log(total);
+// } 
 
-console.log(total);
-=======
-"use strict";
+// console.log(total);
+// "use strict";
 
 // let isUserReady = confirm("Are you ready?");
 // console.log(isUserReady);
@@ -65,17 +64,69 @@ console.log(total);
 
 
 
-let isUserReady = confirm("Are you ready?");
+// let isUserReady = confirm("Are you ready?");
 
-if ( isUserReady ) {
-    console.log("User is ready");
-}
+// if ( isUserReady ) {
+//     console.log("User is ready");
+// }
 
 // if ( isUserReady == false) {
 //     console.log("User is not ready");
 // }
 
-else {
-    console.log("User is not ready");
+// else {
+//     console.log("User is not ready");
+// }
+
+
+// let price = 40;
+// let shippingcost;
+
+// if (price > 50) {
+//     shippingcost = 0;
+// }
+
+// else {
+//     shippingcost = 5;
+// }
+
+// console.log(`Price = ${price}, ShippingCost = ${shippingcost}` );
+
+
+// let shippingcost = price > 50 ? 0 : 5;
+// console.log(`Price = ${price}, ShippingCost = ${shippingcost}` );
+
+
+// let start = confirm("Start?");
+
+// start ? alert("Here you go !") : console.log("Aborted");
+
+// let message = start ? "Here you go !" : "Aborted";
+// alert(message);
+
+
+let gate = prompt("Choose a gate: A, B or C");
+let win = false;
+
+switch (gate) {
+    case "A" : {
+        alert("Gate A : Empty");
+        break;
+    }
+
+    case "B" : {
+        alert("Gate B : Main Prize");
+        win = true;
+        break;
+    }
+
+    case "C" : {
+        alert("Gate C : Empty");
+        break;
+    }
+
 }
->>>>>>> f1e1c1c7d87e5cea605074742f484cf094e30173
+
+if(win) {
+    alert("Winner!");
+}
