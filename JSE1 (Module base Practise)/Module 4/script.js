e1328e10c875022c5aeb6884903724caf7e21564
Jaynob <jynobpapiya12@gmail.com>
2023-05-28 16:18:55 +0600

    Module 4 added

---
+++ JSE1 (Module base Practise)/Module 4/script.js
@@ -0,0 +1,14 @@
+// let isUserReady = confirm("Are you ready?");
+// console.log(isUserReady);
+// if ( isUserReady ) {
+//     alert ("I am ready");
+// }
+
+let UnitPrice = 10;
+let pieces = prompt("How many pieces do you want?", 0);
+if ( pieces > 0 ) {
+    let total = UnitPrice * pieces;
+    console.log(total);
+} 
+
+console.log(total);
